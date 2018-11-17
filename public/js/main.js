49e2f00556e24a4ffc83b5cfce3612a2b1dd7c7e
Sriram Jothiswaran <j.sri.sriram@gmail.com>
2018-11-17 16:45:31 +0530

    Flying Squad search added

--- public/js/main.js
+++ public/js/main.js
@@ -2,40 +2,41 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope, $http) {
   $scope.showSearch = true;
   $scope.assemblyName = ["Jayal", "Khinwsar", "Makrana", "Nawan", "Ladnu", "Deedwana", "Nagaur", "Merta", "Degana", "Parbatsar"];
-  $scope.designation = ["DEO"];
+  $scope.designation = ["DEO", "Flying Squad"];
   $scope.submitSearch = function() {
     if ($scope.selectedDesignation === "DEO") {
       $http({
         method: 'GET',
         url: 'public/json/contacts/deo.json'
       }).then(function(response) {
-        $scope.showSearch = false
+        $scope.showSearch = false;
+        $scope.showDEO = true
         $scope.deoData = response.data
       }, function(error) {
         alert('error occured while fetching data')
       });
 
     }
+
+    if ($scope.selectedDesignation === "Flying Squad") {
+      $http({
+        method: 'GET',
+        url: 'public/json/contacts/flying.json'
+      }).then(function(response) {
+        $scope.showSearch = false;
+        $scope.showSquad = true
+        $scope.squadData = response.data.filter(function(i, n) {
+          return i.assembly === $scope.selectedName;
+        });
+
+
+      }, function(error) {
+        alert('error occured while fetching data')
+      });
+
+    }
+
   }
 
-  app.filter('myTableFilter', function() {
-
-    return function(dataArray, searchTerm) {
-      if (!dataArray) {
-        return;
-      }
-      else if (!searchTerm) {
-        return dataArray;
-      }
-      else {
-        var term = searchTerm.toLowerCase();
-        return dataArray.filter(function(item) {
-          var termInId = item.id.toLowerCase().indexOf(term) > -1;
-          var termInName = item.name.toLowerCase().indexOf(term) > -1;
-          return termInId || termInName;
-        });
-      }
-    }
-  });
 
 });
