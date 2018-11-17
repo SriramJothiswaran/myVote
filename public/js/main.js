692c0b8a70807cb9f180f800b1c53822012d3996
Sriram Jothiswaran <j.sri.sriram@gmail.com>
2018-11-17 18:46:43 +0530

    blo and supervisor search added

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope, $http) {
   $scope.showSearch = true;
   $scope.assemblyName = ["Jayal", "Khinwsar", "Makrana", "Nawan", "Ladnu", "Deedwana", "Nagaur", "Merta", "Degana", "Parbatsar"];
-  $scope.designation = ["DEO", "Flying Squad","Sector Officer","Observer"];
+  $scope.designation = ["DEO", "Flying Squad","Sector Officer","Observer","BLO","Supervisor"];
   $scope.submitSearch = function() {
     if ($scope.selectedDesignation === "DEO") {
       $http({
@@ -75,6 +75,45 @@ app.controller('myCtrl', function($scope, $http) {
     }
 
 
+    if ($scope.selectedDesignation === "BLO") {
+      $http({
+        method: 'GET',
+        url: 'public/json/nagoor.json'
+      }).then(function(response) {
+        $scope.showSearch = false;
+        $scope.showBLO = true
+        $scope.bloData = response.data.data.filter(function(i, n) {
+          return i.assemblyName === $scope.selectedName;
+        });
+
+
+      }, function(error) {
+        alert('error occured while fetching data')
+      });
+
+    }
+
+    if ($scope.selectedDesignation === "Supervisor") {
+      $http({
+        method: 'GET',
+        url: 'public/json/nagoor.json'
+      }).then(function(response) {
+        $scope.showSearch = false;
+        $scope.showSupervisor = true
+        $scope.supervisorData = response.data.data.filter(function(i, n) {
+          return i.assemblyName === $scope.selectedName;
+        });
+
+
+      }, function(error) {
+        alert('error occured while fetching data')
+      });
+
+    }
+
+
+
+
   }
 
   $scope.goSearch = function(){
@@ -82,6 +121,8 @@ app.controller('myCtrl', function($scope, $http) {
     $scope.showSector = false;
     $scope.showSquad = false;
     $scope.showDEO = false;
+    $scope.showBLO = false;
+    $scope.showSupervisor = false;
     $scope.showSearch = true;
   }
 
